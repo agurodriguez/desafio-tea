c1254ea4f48bdb28312a44964fba8961cc6342e8
Agustín Rodríguez <me@agurodriguez.net>
2018-10-21 00:34:47 -0300

    mejor manejo de memoria

--- scripts/pasadas.js
+++ scripts/pasadas.js
@@ -6,16 +6,22 @@ const http = require('http');
  * e intentar de nuevo sin tener que volver a descargar todos los 
  * datos de nuevo
  */
-let k = 0;
 const MAX_LINEA = 494;
 const MAX_PARADA = 6318;
-for (let i = 0; i < MAX_PARADA; i++) {
-    for (let j = 0; j < MAX_LINEA; j++) {
-        // demoremos 10ms cada una de las request para no atomizar al endpoint
-        // el script debería demorar aprox (494 * 6318 * 10) ms en terminar
-        // (494 * 6318 * 10) ms ~= 520 mins = 8 horas, 40 mins
-        setTimeout(() => {
 
+function sleep(t) {
+    return new Promise(resolve => setTimeout(resolve, t));
+}
+
+(async function () {
+
+    let k = 0;
+
+    for (let i = 0; i < MAX_PARADA; i++) {
+        for (let j = 0; j < MAX_LINEA; j++) {
+            // demoremos 10ms cada una de las request para no atomizar al endpoint
+            // el script debería demorar aprox (494 * 6318 * 10) ms en terminar
+            // (494 * 6318 * 10) ms ~= 520 mins = 8 horas, 40 mins
             let req = http.get(`http://www.montevideo.gub.uy/transporteRest/pasadas/${i}/HABIL/${j}`, (res) => {
                 let data = '';
                 res.on('data', chunk => data += chunk);
@@ -30,6 +36,8 @@ for (let i = 0; i < MAX_PARADA; i++) {
                 // console.log(err);
             });
 
-        }, 10 * k++);
+            await sleep(10 * k++);
+        }
     }
-}
+
+})();
