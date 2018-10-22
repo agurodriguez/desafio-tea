910034484e16700365a1daf05266f249293c2e73
Gastón Donadío <tongadonadio@gmail.com>
2018-10-21 23:40:14 -0300

    Se agregan funciones de distancia

--- src/tea.js
+++ src/tea.js
@@ -3,36 +3,68 @@ const orion = require('./services/orion');
 const PUBLIC_URL = 'https://592f2662.ngrok.io';
 
 class Tea {
+  constructor() {
+    this.busLocationChangesSubscription = undefined;
+  }
 
-    constructor() {
-        this.busLocationChangesSubscription = undefined;
+  getNextBusForBusStop() {}
+
+  getLastBusForBusStop(busStopId, bus) {}
+
+  getTimeBetweenTwoPointsForBus(bus, fromPoint, toPoint) {}
+
+  handleOrionAccumulate(body) {
+    if (body.subscriptionId == this.busLocationChangesSubscription.id) {
+      body.data.forEach(i => console.log(i));
     }
+  }
 
-    getNextBusForBusStop() {
-    }
-    
-    getLastBusForBusStop(busStopId) {
-    }
+  run() {
+    orion
+      .subscribeToBusLocationChanges('62', `${PUBLIC_URL}/orion/accumulate`)
+      .then(body => {
+        this.busLocationChangesSubscription = body.subscription;
+      })
+      .catch(err => console.log(err));
+  }
 
-    getTimeBetweenTwoPointsForBus(bus, fromPoint, toPoint) {
+  // start and end are objects with latitude and longitude
+  //decimals (default 2) is number of decimals in the output
+  //return is distance in kilometers.
+  getDistance = function(start, end, decimals) {
+    decimals = decimals || 2;
+    var earthRadius = 6371; // km
+    lat1 = parseFloat(start.latitude);
+    lat2 = parseFloat(end.latitude);
+    lon1 = parseFloat(start.longitude);
+    lon2 = parseFloat(end.longitude);
 
-    }
+    var dLat = (lat2 - lat1).toRad();
+    var dLon = (lon2 - lon1).toRad();
+    var lat1 = lat1.toRad();
+    var lat2 = lat2.toRad();
 
-    handleOrionAccumulate(body) {
-        if (body.subscriptionId == this.busLocationChangesSubscription.id) {
-            body.data.forEach(i => console.log(i));
-        }
-    }
+    var a =
+      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+      Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    var d = earthRadius * c;
+    return Math.round(d * Math.pow(10, decimals)) / Math.pow(10, decimals);
+  };
 
-    run() {
-        orion
-            .subscribeToBusLocationChanges('62', `${PUBLIC_URL}/orion/accumulate`)
-            .then(body => {
-                this.busLocationChangesSubscription = body.subscription
-            })
-            .catch(err => console.log(err));
-    }
-
+  getClosest = (points, busStop) => {
+    var distance = -1;
+    var result;
+    Array.prototype.forEach.call(points, p => {
+      let end = { latitude: p['lat'], longitude: p['lon'] };
+      let distanceAux = Distance.getDistance(busStop, end);
+      if (distance === -1 || distance > distanceAux) {
+        result = p;
+        distance = distanceAux;
+      }
+    });
+    return result;
+  };
 }
 
 module.exports = new Tea();
