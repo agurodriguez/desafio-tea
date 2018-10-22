0eb56ed7bcc67b220c93d94622101eb5d419995e
Agustín Rodríguez <me@agurodriguez.net>
2018-10-21 22:30:56 -0300

    subscipción a orion para obtener los cambios de ubicación de los buses

--- src/index.js
+++ src/index.js
@@ -1,16 +1,32 @@
 const express = require('express');
-const app = express();
-
+const orion = require('./services/orion');
 const tea = require('./services/tea');
+
+const PUBLIC_URL = 'https://592f2662.ngrok.io';
+
+let app = express();
+
+app.use(require('body-parser').urlencoded({ extended: false }));
+app.use(require('body-parser').json());
 
 app.get('/nextBus/:busLine/:busStopId', function (req, res) {
     let nextBusForBusStop = tea.getNextBusForBusStop(req.params.busLine, req.params.busStopId);
     let lastBusForBusStop = tea.getLastBusForBusStop(req.params.busLine, req.params.busStopId);
     let time = tea.getTimeBetweenTwoPointsForBus(lastBusForBusStop, [-56.4333, 32.8989], [-56.4333, 32.8989]);
-    
     res.send(req.params);
+});
+
+app.all('/orion/accumulate*', function (req, res) {
+    console.log('accumulate');
+    console.log(req.body);
+    res.sendStatus(200);
 });
 
 app.listen(8080, function () {
     console.log('Example app listening on port 8080!');
 });
+
+orion
+    .subscribeToLocationChanges('62', `${PUBLIC_URL}/orion/accumulate`)
+    .then(body => console.log(body))
+    .catch(err => console.log(err));
--- src/services/orion.js
+++ src/services/orion.js
@@ -1,6 +1,35 @@
+const moment = require('moment');
+const ngsi = require('ngsijs');
+const request = require('request-promise');
+
 class Orion {
 
-    apiUrl = 'https://kobauy.ddns.net:1026';
+    constructor() {
+        this.client = new ngsi.Connection('http://kobauy.ddns.net:1026');
+    }
+
+    subscribeToLocationChanges(busId, callbackUrl) {
+        return this.client.v2.createSubscription({
+            description: `A subscription to get location updates from bus ${busId}`,
+            subject: {
+                entities: [
+                    {
+                        'idPattern': '.*',
+                        'type': 'Bus'
+                    }
+                ]
+            },
+            notification: {
+                http: {
+                    url: callbackUrl
+                },
+                attr: [
+                    'location'
+                ]
+            },
+            expires: moment().add('2 hours').toISOString()
+        });
+    }
 
 }
 
