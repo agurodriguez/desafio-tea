d10263b0c68af409556b0a76e670cf128d75c678
Agustín Rodríguez <me@agurodriguez.net>
2018-10-24 10:26:19 -0300

    socket para servir los eventos recibidos de orion 'en vivo' al playground

--- src/index.js
+++ src/index.js
@@ -5,25 +5,33 @@ if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
 const express = require('express');
 const tea = require('./tea');
 
-let app = express();
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
 app.use(require('body-parser').urlencoded({ extended: false }));
 app.use(require('body-parser').json());
 
 app.get('/nextBus/:busVariant/:busStopId', async function (req, res) {
-    res.send(tea.getNextBusForBusStopEta(req.params.busVariant, req.params.busStopId));
+    tea.getNextBusForBusStopEta(req.params.busVariant, req.params.busStopId).then(eta => res.send(eta))
 });
 
 app.post('/orion/accumulate', function (req, res) {
     tea.handleOrionAccumulate(req.body);
     res.sendStatus(200);
+    io.emit('orion/accumulate', req.body);
 });
 
-app.listen(process.env.PORT, function () {
+io.on('connection', function (socket) {
+    console.log(`${socket} connected`);
+});
+
+server.listen(process.env.PORT, function () {
     console.log(`koba-tea running on port ${process.env.PORT}`);
 });
 
 tea.run();
 
-tea.getNextBusForBusStopEta(7921, 2859)
-    .then(res => console.log(res));
+// tea.getNextBusForBusStop(7920, 2837).then(console.log);
+// tea.getLastBusForBusStop(7920, 2959).then(console.log)
+// tea.getNextBusForBusStopEta(7920, 3219).then(console.log);
