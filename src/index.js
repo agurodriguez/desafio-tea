c4dae90b2e158087d18f96add62944e52797127f
Agustín Rodríguez <me@agurodriguez.net>
2018-10-24 23:54:41 -0300

    corregidos los problemas con cors

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
 }
 
 const express = require('express');
+const corsAnywhere = require('cors-anywhere');
 const tea = require('./tea');
 
 var app = express();
@@ -44,6 +45,8 @@ app.get('/test/getTimeBetweenTwoBusStopsForBus/:busVariant/:busId/:busStopId1/:b
     tea.getTimeBetweenTwoBusStopsForBus(req.params.busVariant, req.params.busId, req.params.busStopId1, req.params.busStopId2).then(r => res.send(r.toString())).catch(err => res.send(err));
 });
 
+app.use('/playground', express.static('playground'));
+
 io.on('connection', function (socket) {
     console.log(`${socket.handshake.address} connected`);
     socket.on('disconnect', function () {
@@ -53,6 +56,10 @@ io.on('connection', function (socket) {
 
 server.listen(process.env.PORT, function () {
     console.log(`koba-tea running on port ${process.env.PORT}`);
+});
+
+corsAnywhere.createServer().listen(+process.env.PORT + 1, function () {
+    console.log(`koba-cors-anywhere running on port ${+process.env.PORT + 1}`);
 });
 
 tea.events.on('busesLocations', (locations) => {
